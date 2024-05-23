9f4e8d49c375191ad308d29945db6e70135b5d6b
Jan Diehm <jadiehm@gmail.com>
2024-05-23 17:17:40 -0500

    tag wrangling

--- tasks/combine-tags.js
+++ tasks/combine-tags.js
@@ -5,8 +5,7 @@ const files = fs.readdirSync(INPATH);
 const categoriesRaw = fs.readFileSync("static/assets/1-tagged-relationships.csv", "utf8");
 const categoriesCSV = d3.csvParse(categoriesRaw);
 
-let groupedData = [];
-let singleShipData = [];
+let shipData = [];
 
 function pause(delay) {
     return new Promise(resolve => {
@@ -14,82 +13,82 @@ function pause(delay) {
     })
 }
 
-function saveCSV(data) {
-    let relationship = (data[0].relationship).replaceAll(" ", "-").replaceAll("/", "_");
+function findMatch(file, key) {
+    let match;
+    if (key == "relationship") {
+        match = categoriesCSV.find((element) => element.fileName == file).relationship;
+    } else if (key == "relType") {
+        match = categoriesCSV.find((element) => element.fileName == file).relType;
+    } else if (key == "isCanon") {
+        match = categoriesCSV.find((element) => element.fileName == file).isCanon;
+    } else if (key == "isRPF") {
+        match = categoriesCSV.find((element) => element.fileName == file).isRPF;
+    }
+    return match
+}
+
+function organize(file) {
+    const raw = fs.readFileSync(`${INPATH}${file}`, "utf8");
+    const csvData = d3.csvParse(raw);
+    const concatData = [].concat(...csvData).map(d => ({
+		...d,
+        fileName: file,
+        taggedRel: findMatch(file, "relationship"),
+        relType: findMatch(file, "relType"),
+        isCanon: findMatch(file, "isCanon"),
+        isRPF: findMatch(file, "isRPF")
+
+	}));
+    const groupedData = d3.groups(concatData, d => d.title); 
+
+    groupedData.map(combineWorks)
+    saveCSV(shipData, findMatch(file, "relationship"))
+}
+
+function saveCSV(data, taggedRel) {
+    let relationship = (taggedRel).replaceAll(" ", "-").replaceAll("/", "_");
     console.log(relationship)
 
     let concatData = [].concat(...data).map(d => ({
         ...d
     }));
     let csv = d3.csvFormat(data);
-    fs.writeFileSync(`tasks/output/${relationship}.csv`, csv);
+    fs.writeFileSync(`tasks/output/combinedTags/${relationship}.csv`, csv);
 }
 
-async function groupData(file) {
-    const raw = fs.readFileSync(`${INPATH}${file}`, "utf8");
-    const csvData = d3.csvParse(raw);
-    groupedData = d3.groups(csvData, d => d.title);  
+function combineWorks(data,i) {
+    const fandom = data[1][0].fandom;
+    const relationship = data[1][0].relationship;
+    const taggedRel = data[1][0].taggedRel;
+    const relType = data[1][0].relType;
+    const isCanon = data[1][0].isCanon;
+    const isRPF = data[1][0].isRPF;
+    const title = data[0];
+    const datetime = data[1].filter(d => d.datetime !== "")[0].datetime;
+    const kudos = data[1].filter(d => d.kudos !== "")[0].kudos;
+    const rating = data[1].filter(d => d.rating !== "")[0].rating;
+    let warnings = data[1].filter(d => d.warnings !== "");
+    warnings = warnings.map(a => a.warnings).toString();
+    let tags = data[1].filter(d => d.tags !== "");
+    tags = tags.map(a => a.tags).toString();
+
+    shipData.push({fandom, relationship, taggedRel, title, relType, isCanon, isRPF, datetime, kudos, rating, warnings, tags})
 }
 
-async function buildAggregate(data, file) {
-
-    for (const work of data[1]) {
-        try {
-            await getKeyValues(work[1], file);
-        } 
-        catch (err) {
-            console.log(err);
-        }
-        // await saveCSV(singleShipData);
-        // await pause(100);
-    }
+function clearData() {
+    shipData = [];
 }
 
-async function getKeyValues(data, file) {
-    console.log(data)
-
-    // const fandom = data[0].fandom;
-    // const relationship = data[0].relationship;
-    // const title = data[0];
-    // console.log(relationship)
-    // const datetime = data.filter(d => d.datetime !== "")[0].datetime;
-    // const kudos = data.filter(d => d.kudos !== "")[0].kudos;
-    // const rating = data.filter(d => d.rating !== "")[0].rating;
-    // let warnings = data.filter(d => d.warnings !== "");
-    // warnings = warnings.map(a => a.warnings).toString();
-    // let tags = data.filter(d => d.tags !== "");
-    // tags = tags.map(a => a.tags).toString();
-    // const match = categoriesCSV.find((element) => element.fileName == file)
-    // const relType = match.relType;
-    // const isCanon = match.isCanon;
-    // const isRPF = match.isRPF;
-    // singleShipData.push({fandom, relationship, relType, isCanon, isRPF, title, datetime, kudos, rating, warnings, tags})
-}
-
-async function buildOutput() {
-    // const datetime = data.filter(d => d.datetime !== "")[0].datetime;
-    // const kudos = data.filter(d => d.kudos !== "")[0].kudos;
-    // const rating = data.filter(d => d.rating !== "")[0].rating;
-    // let warnings = data.filter(d => d.warnings !== "");
-    // warnings = warnings.map(a => a.warnings).toString();
-    // let tags = data.filter(d => d.tags !== "");
-    // tags = tags.map(a => a.tags).toString();
-    // const match = categoriesCSV.find((element) => element.fileName == file)
-    // const relType = match.relType;
-    // const isCanon = match.isCanon;
-    // const isRPF = match.isRPF;
-    // singleShipData.push({fandom, relationship, relType, isCanon, isRPF, title, datetime, kudos, rating, warnings, tags})
-}
 
 (async () => {
     for (const file of files) {
         try {
-            await groupData(file);
-            await buildAggregate(groupedData, file);
-            await pause(1000);
-        } 
+            await organize(file)
+            await clearData()
+        }
         catch (err) {
             console.log(err);
         }
+        await pause(5000);
     }
 })();
