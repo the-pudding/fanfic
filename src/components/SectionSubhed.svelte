b313a9159659c1f3a8e40cde3feae6354d1ac68c
Jan Diehm <jadiehm@gmail.com>
2024-10-09 11:05:12 -0500

    with robust sparkle and script comments

--- src/components/SectionSubhed.svelte
+++ src/components/SectionSubhed.svelte
@@ -1,21 +1,30 @@
 <script>
+    // This is the headline text passed in from the parent component, FanFicSection.svelte
     export let copy;
 
+    // This splits the text at every space, giving us an array of words
     const wordSplit = copy.split(" ");
-    const charSplit = copy.split("");
 
-    function checkVowel(char) {
+    // Check if the char is included in the letter array below
+    function checkChar(char) {
+        // Change all characters to lowercase
         char = char.toLowerCase();
+        // Return true/false if the character is included in the array of letters below
+        // EXAMPLE: 'a' would return true, but 's' would return false
         return ['a', 'e', 'o'].includes(char);
     }
 </script>
 
 
 <h3>
+    <!-- For each word in the wordSplit array, create a span -->
     {#each wordSplit as word, i}
         <span class="word">
+            <!-- For each character in the word, split again and create a span -->
             {#each word.split("") as char, i}
-                {@const isVowel = checkVowel(char)}
+                <!-- Check if the char is a included in the letter array inside the checkChar() function, and if it is set isVowel to true -->
+                {@const isVowel = checkChar(char)}
+                <!-- Set the span class by isVowel so you get "hedspan-true" or "hedspan-false", then style it accordingly in the css -->
                 <span class="char hedspan-{isVowel}">{char}</span>
             {/each}
         </span>
@@ -26,12 +35,12 @@
 <style>
     h3 {
         font-size: var(--80px);
-        line-height: 1.125;
-        margin: 2rem auto 0 auto;
-        text-transform: uppercase;
         font-family: var(--sans);
         font-weight: 300;
+        text-transform: uppercase;
         text-align: center;
+        line-height: 1.125;
+        margin: 2rem auto 0 auto;
         color: var(--fanfic-highlighter);
     }
 
--- src/components/Sparkles.svelte
+++ src/components/Sparkles.svelte
@@ -1,21 +1,33 @@
 <script>
+    // Make an array of numbers between 1-20, change 20 to be any number
     const sparkleArray = Array.from({length: 20}, (_, index) => index + 1);
+    
+    // These are properties passed in from the parent component, IntroSection.svelte
     export let introW;
     export let introH;
 
+    // This generates a random placement for the sparkles based on the pixel width and height of the parent container
+    // EXAMPLE: if pixels is 100, then the possible return values would be 0, 20, 40, 60, 80, or 100
     function generateRandomPos(pixels) {
+        // Since the sparkles themselves are 20px, we need to find the maximum number of times they can fit in the width/height so none overlap
         const maxMultiple = Math.floor(pixels / 20);
+        // This selects a random integer from 0 to the maxMultiple, (maxMultiple + 1) ensures maxMultiple is included, and Math.floor rounds down to the nearest whole number 
         const randomMultiple = Math.floor(Math.random() * (maxMultiple + 1));
+        // This returns the pixel placement of the sparkle (random multiple * the sparkle width of 20px)
         return randomMultiple * 20;
     }
 
+    // This generates a random duration for the sparkle spin
+    // EXAMPLE: since max is 3, the possible return values would be 2, 3, or 4
     function generateRandomDur(max) {
+        // This generates a random decimal between 0-1, then scales that range by the max and insures that the value of the integer is not les than 2
         const random = Math.floor(Math.random() * max)+2;
         return random;
     }
 </script>
 
 <div class="sparkles" >
+    <!-- For each number/sparkle (20) in the sparklesArray, add an image with unique styles -->
     {#each sparkleArray as sparkle}
         <img 
             style="top:{generateRandomPos(introH)}px; 
